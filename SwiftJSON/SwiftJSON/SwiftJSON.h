dcb8a5980c44dc66ce1a87dec819146868d4efde
Naohiro Hamada <nao.pinguinus@icloud.com>
2016-01-18 20:37:58 +0900

    Initial Update

---
+++ SwiftJSON/SwiftJSON/SwiftJSON.h
@@ -0,0 +1,19 @@
+//
+//  SwiftJSON.h
+//  SwiftJSON
+//
+//  Created by Naohiro Hamada on 2016/01/18.
+//  Copyright © 2016年 Naohiro Hamada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftJSON.
+FOUNDATION_EXPORT double SwiftJSONVersionNumber;
+
+//! Project version string for SwiftJSON.
+FOUNDATION_EXPORT const unsigned char SwiftJSONVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftJSON/PublicHeader.h>
+
+
